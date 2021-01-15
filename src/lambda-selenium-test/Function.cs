4525527488ca7dc25dd11b0a77d94d7831262d79
Bob Libby <bob.libby@lifetimebrands.com>
2021-01-15 10:23:32 -0500

    Init

---
+++ src/lambda-selenium-test/Function.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace lambda_selenium_test
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and returns both the upper and lower case version of the string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(FunctionInput input, ILambdaContext context)
+        {
+            Console.WriteLine($"input.Url: {input.Url.ToString()}");
+            
+            // Setup Selenium driver
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
+            chromeOptions.AddArgument("--no-sandbox");
+            chromeOptions.AddArgument("--headless");
+            chromeOptions.AddArgument("--verbose");
+            chromeOptions.AddArgument("--disable-dev-shm-usage");
+
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+
+            driver.Navigate().GoToUrl(input.Url);
+            return input.Url.ToString();
+        }
+    }
+
+    public record Casing(string Lower, string Upper);
+
+    public class FunctionInput
+    {
+        public Uri Url { get; set; }
+    }
+}
